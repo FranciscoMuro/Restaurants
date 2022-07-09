2fe10433fa66fa97354f471b3fe8c8d057e65ffb
Franciscomuro <francisco.muro.se@gmail.com>
2022-07-08 23:23:33 -0500

    add crud for the restaurants model

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import fill_db
+from .views import (fill_db, getRestaurants, postRestaurant,
+                    updateRestaurant, deleteRestaurant)
 
 app_name = 'restaurants'
 
 urlpatterns = [
     path('fill-db/', fill_db, name='fill_db'),
+    path('', getRestaurants, name='getRestaurants'),
+    path('post-restaurant/', postRestaurant, name='postRestaurant'),
+    path('update-restaurant/<str:id>', updateRestaurant, name='updateRestaurant'),
+    path('delete-restaurant/<str:id>', deleteRestaurant, name='deleteRestaurant'),
 ]
