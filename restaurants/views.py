c4a0926a6d4d563c91b655d5d29dbb931bb9fa78
Franciscomuro <francisco.muro.se@gmail.com>
2022-07-08 22:03:36 -0500

    add comments to the new function

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,17 +1,20 @@
-import email
 from django.http import JsonResponse
 import pandas as pd
 from django.views.decorators.csrf import csrf_exempt
 from .models import restaurant
 
+# We use pandas to read the csv
 @csrf_exempt
 def fill_db(request):
+    # here we see if we have the right file
     csvFile = request.FILES['restaurants']
     if not csvFile.name.endswith('.csv'):
         return JsonResponse('The file selected is not a csv file', safe=False)
+    # Read the csv file with pandas an turn it into a data frame to get the values of the csv
     restaurantCsv = pd.read_csv(csvFile)
     restaurantDf = pd.DataFrame(restaurantCsv)
     for restaurantIterator in restaurantDf.itertuples():
+        # Here we make an iteration of the data frame and turn it into a restaurant object to save it in the db.
         restaurantModel = restaurant(
             id=restaurantIterator.id,
             rating=restaurantIterator.rating,
